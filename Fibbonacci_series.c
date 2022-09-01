22cebfeff72bc1d553623d217b8b6c417fe18751
S Meshram <109426976+codelearner619@users.noreply.github.com>
2022-09-01 20:13:35 +0530

    Add files via upload

---
+++ Fibbonacci_series.c
@@ -0,0 +1,33 @@
+
+
+#include <stdio.h>
+int
+main ()
+{
+
+  int i, n;
+
+
+  int t1 = 0, t2 = 1;
+
+
+  int nextTerm = t1 + t2;
+
+
+  printf ("Enter the number of terms: ");
+  scanf ("%d", &n);
+
+
+  printf ("Fibonacci Series:\n %d\n %d\n ", t1, t2);
+
+
+  for (i = 3; i <= n; ++i)
+    {
+      printf ("%d\n ", nextTerm);
+      t1 = t2;
+      t2 = nextTerm;
+      nextTerm = t1 + t2;
+    }
+
+  return 0;
+}
